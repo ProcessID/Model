aacaabebce4e23abccb33862a5dbc79af8b425e4
processid <34098270+processid@users.noreply.github.com>
2020-07-08 16:18:50 +0200

    Add files via upload

--- Model.php
+++ Model.php
@@ -1,66 +1,54 @@
 <?php
-/*
-   // Classe de gestion d'un objet
-   // Cette classe doit être héritée
-   // La classe apporte tous les setters et les getters des attributs
-   // C'est une instance de cette classe qui sera passée au manager
-   // Voici un exemple de classe enfant:
+    /*
+    // Classe de gestion d'un objet
+    // Cette classe doit être héritée
+    // La classe apporte tous les setters et les getters des attributs
+    // C'est une instance de cette classe qui sera passée au manager
+    // Voici un exemple de classe enfant:
 
-   class Clients extends \ProcessID\Model\Model {
-      protected $IDclients;
-      protected $nom;
-      protected $tel;
-      protected $siret;
-   }
-   
-   
-*/   
-   namespace processid\model;
+    class Clients extends \ProcessID\Model\Model {
+    protected $IDclients;
+    protected $nom;
+    protected $tel;
+    protected $siret;
+    }
 
-   abstract class Model  {
-      
-      // >=PHP 5.4:
-      //use ProcessID\Traits\Hydrate;
-      
-      private $ta_setters;
-      private $ta_getters;
-      
-      function __construct($data) {
-         $this->createSettersGetters();
-         $this->hydrate($data);
-      }
-      
-      // <PHP 5.4
-      protected function hydrate(array $data) {
-         foreach ($data as $key => $value) {
-            $method = 'set'.ucfirst($key);
-            if (is_callable(array($this, $method))) {
-               $this->$method($value);
+
+    */   
+    namespace processid\model;
+
+    abstract class Model  {
+        use \processid\traits\Hydrate;
+
+        private $ta_setters;
+        private $ta_getters;
+
+        function __construct($data) {
+            $this->createSettersGetters();
+            $this->hydrate($data);
+        }
+
+        protected function createSettersGetters() {
+            $this->ta_setters = array();
+            $this->ta_getters = array();
+
+            foreach ($this as $attribut=>$valeur) {
+                $this->ta_setters['set' . ucfirst($attribut)] = $attribut;
+                $this->ta_getters[$attribut] = $attribut;
             }
-         }
-      }
-      
-      protected function createSettersGetters() {
-         $this->ta_setters = array();
-         $this->ta_getters = array();
-         
-         foreach ($this as $attribut=>$valeur) {
-            $this->ta_setters['set' . ucfirst($attribut)] = $attribut;
-            $this->ta_getters[$attribut] = $attribut;
-         }
-      }
-      
-      function __call($name,$arg) {
-         if (array_key_exists($name,$this->ta_setters)) {
-            // Setter
-            $this->{$this->ta_setters[$name]} = $arg[0];
-         }
-         
-         if (array_key_exists($name,$this->ta_getters)) {
-            // Getter
-            return $this->{$name};
-         }
-         
-      }
-   }
+        }
+
+        function __call($name,$arg) {
+            if (array_key_exists($name,$this->ta_setters)) {
+                // Setter
+                $this->{$this->ta_setters[$name]} = $arg[0];
+            }
+
+            if (array_key_exists($name,$this->ta_getters)) {
+                // Getter
+                return $this->{$name};
+            }
+
+        }
+    }
 ?>
